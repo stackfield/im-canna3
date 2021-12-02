5c7f3ba69dc895ff2066a421b1ae0a03222c8303
stackfield <stackfield0trpg@gmail.com>
2021-12-02 09:02:19 +0900

    clean up im_canna_function_mode.

--- sub_function_mode.c
+++ sub_function_mode.c
@@ -17,6 +17,7 @@
 gboolean im_canna_function_mode (GtkIMContext *context, GdkEventKey *key) {
   IMContextCanna *cn = IM_CONTEXT_CANNA(context);
   guchar canna_code = 0;
+  gint nbytes = 0;
 
   if (im_canna_is_key_of_no_use_in_canna(key))
     return FALSE;
@@ -27,25 +28,18 @@ gboolean im_canna_function_mode (GtkIMContext *context, GdkEventKey *key) {
     canna_code = get_canna_keysym(key->keyval, key->state);
   }
 
-  if (canna_code != 0) {
-    gint nbytes;
-
+  if (canna_code != 0)
     nbytes = jrKanjiString(cn->canna_context, canna_code, cn->kakutei_buf, BUFSIZ, &cn->ks);
-
-    if ((nbytes >= 2)) {
-      gchar* euc = g_strndup(cn->kakutei_buf, nbytes);
-      gchar* utf8 = euc2utf8(euc);
-      g_signal_emit_by_name(cn, "commit", utf8);
-      g_free(utf8);
-      g_free(euc);
-    }
-
-    return TRUE;
-  } else {
-    gint nbytes;
-
+  else
     nbytes = jrKanjiString(cn->canna_context, key->keyval, cn->kakutei_buf, BUFSIZ, &cn->ks);
+
+  if (nbytes >= 2) {
+    gchar* euc = g_strndup(cn->kakutei_buf, nbytes);
+    gchar* utf8 = euc2utf8(euc);
+    g_signal_emit_by_name(cn, "commit", utf8);
+    g_free(utf8);
+    g_free(euc);
   }
-  
+
   return TRUE;
 }
