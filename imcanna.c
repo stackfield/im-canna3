3b94f35a3cbef8baf35aae8c6c9aa3b93300595b
stackfield <stackfield0trpg@gmail.com>
2021-12-07 02:13:44 +0900

    refactor focusout/reset and reconnect canna when prevkeytime == 0.

--- imcanna.c
+++ imcanna.c
@@ -328,12 +328,13 @@ im_canna_filter_keypress(GtkIMContext *context, GdkEventKey *key)
     if( key->time - cn->prevkeytime >= KEY_TIMEOUT ||
 	cn->prevkeytime < key->time ) {
       if( cn->preedit_length == 0 && cn->gline_length == 0 ) {
-	if( cn->ja_input_mode == TRUE &&
-	    strcmp(cn->init_mode_string, cn->modebuf) == 0 ) {
-	  im_canna_disable_ja_input_mode(context);
-	  im_canna_enable_ja_input_mode(context);
-	  gtk_widget_show_all(cn->modewin);
-	}
+	if( cn->ja_input_mode == TRUE )
+	  if( strcmp(cn->init_mode_string, cn->modebuf) == 0 ||
+	      cn->prevkeytime == 0) {
+	    im_canna_disable_ja_input_mode(context);
+	    im_canna_enable_ja_input_mode(context);
+	    gtk_widget_show_all(cn->modewin);
+	  }
       }
     }
   }
@@ -528,6 +529,7 @@ im_canna_focus_out (GtkIMContext* context) {
     }
 
     cn->prevkeytime = 0;
+    clear_gline(cn);
     gtk_widget_hide(GTK_WIDGET(cn->modewin));
     gtk_widget_hide(GTK_WIDGET(cn->candwin));
   }
@@ -593,6 +595,7 @@ im_canna_reset(GtkIMContext* context) {
     }
     
     cn->prevkeytime = 0;
+    clear_gline(cn);
     gtk_widget_hide(GTK_WIDGET(cn->modewin));
     gtk_widget_hide(GTK_WIDGET(cn->candwin));
   }
